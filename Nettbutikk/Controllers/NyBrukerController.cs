c3fe4f05f2c667cdda992e4c6f46e00e44947433
Amin Alizadeh <156803041+KingAminRn@users.noreply.github.com>
2024-12-19 13:44:54 +0100

    new
    legget til ny funksjon upload prioduct og procedure

--- Nettbutikk/Controllers/NyBrukerController.cs
+++ Nettbutikk/Controllers/NyBrukerController.cs
@@ -22,15 +22,19 @@ namespace Nettbutikk.Controllers
         }
 
         [HttpPost]
-        public IActionResult NyBruker(NyBurkerM objBukerM)
+        public IActionResult Bruker(NyBurkerM objBukerM)
         {
 
             string conStr = "Data Source=.\\SQLEXPRESS; Initial Catalog=Nettbutikkk_Amin; Integrated Security=True";
             SqlConnection con = new SqlConnection(conStr);
             con.Open();
 
-            string qury = "insert into logininfo values ('" + objBukerM.Username + "', '" + objBukerM.Password + "')";
-            SqlCommand cmd = new SqlCommand(qury, con);
+            //string qury = "insert into logininfo values ('" + objBukerM.Username + "', '" + objBukerM.Password + "')";
+            SqlCommand cmd = new SqlCommand("spSaveLogininfo", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@username", objBukerM.Username);
+            cmd.Parameters.AddWithValue("@password", objBukerM.Password);
+
             cmd.ExecuteNonQuery();
 
             return View(objBukerM);
